2d862ee0edb52aac0be4103d2d898ce12eaa39e7
Shashikant-15 <shashikant.tanti@knoldus.com>
2023-01-18 02:08:54 +0530

    final commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## resilience4j Examples
+
+>>demo of spring boot resilience integration example.
---
+++ SecondService/src/main/java/com/knoldus/secondservice/controller/SecondServiceController.java
@@ -0,0 +1,16 @@
+package com.knoldus.secondservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/b")
+public class SecondServiceController {
+
+    @GetMapping
+    public String serviceB() {
+        return "2nd Service is called from FirstService ";
+    }
+
+}
